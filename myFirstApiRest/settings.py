86ebefe865fdb3a48b886fd2662d6416bb5719e0
enriquerodriguezcamacho <202213390@alu.comillas.edu>
2025-04-16 20:23:30 +0200

    Ultimo commit

--- myFirstApiRest/settings.py
+++ myFirstApiRest/settings.py
@@ -32,7 +32,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '@dpg-cvrbaqmr433s73b7p0eg-a.oregon-postgres.render.com', 'lospujantesbackend-1.onrender.com']
 
-
 # Application definition
 
 INSTALLED_APPS = [
