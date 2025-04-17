c32834f81e9946bf086749c045c3514a49ff7fba
Pablo Díaz Vega <pablodiazvega4@gmail.com>
2025-04-17 22:27:24 +0200

    url change

--- myFirstApiRest/urls.py
+++ myFirstApiRest/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-
+from django.shortcuts import redirect
 
 
 urlpatterns = [
@@ -29,4 +29,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path("", lambda request: redirect("/api/")),
 ]
