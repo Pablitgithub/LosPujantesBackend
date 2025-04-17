5c285e445ec70d4f56e53e81352a4f4f6ca9da48
Pablo Díaz Vega <pablodiazvega4@gmail.com>
2025-04-17 22:40:36 +0200

    fix: redirect /api/ to /api/auctions/

--- myFirstApiRest/urls.py
+++ myFirstApiRest/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path("", lambda request: redirect("/api/")),
+    path("api/", lambda request: redirect("/api/auctions/"), name="api-root"),
 ]
