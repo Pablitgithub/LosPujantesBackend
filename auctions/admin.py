fa441fd78a24b5eb58d8ae2ff088c762ca92b460
Pablo Díaz Vega <pablodiazvega4@gmail.com>
2025-05-11 12:19:56 +0200

    Registrar los nuevos modelos en el admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Category, Auction, Bid, Rating
+from django.utils import timezone
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+@admin.register(Auction)
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "auctioneer", "price", "is_open")
+    list_filter = ("category", "auctioneer", "closing_date")
+    search_fields = ("title", "description")
+    readonly_fields = ("creation_date",)
+    
+    def is_open(self, obj):
+        return obj.closing_date > timezone.now()
+    is_open.boolean = True
+    is_open.short_description = "Abierta?"
+
+@admin.register(Bid)
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "auction", "bidder", "price", "creation_date")
+    list_filter = ("auction", "bidder")
+    readonly_fields = ("creation_date",)
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ("id", "auction", "user", "value", "created")
+    list_filter = ("auction", "user", "value")
+    readonly_fields = ("created",)
